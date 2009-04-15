ece8e3eb3ac17a684cde1aa9198c5764a73f3031
Jim Hayes <jhayes@sdsc.edu>
2009-04-15 23:22:09 +0000

    Misc. corrections.

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -8,6 +8,6 @@ SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
 SUMMARY_ARCHITECTURE    = i386, x86_64
 
-ROLL_REQUIRES           =
+ROLL_REQUIRES           = base kernel os1 os2 web-server
 ROLL_CONFLICTS          = 
 
