0d30959630d3cff19a1f2cc70c2d02e693a8fae6
Mason Katz <mason.katz@gmail.com>
2011-02-14 23:34:13 +0000

    NOTICE: Rocks build environment has changed
    You must rebuild and install the rocks-devel package (base roll).  Only
    then can you build docs on your frontend.
    
    All usersguides have been updated to build with the new rules.

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,4 +1,3 @@
-ROLL    = nagios
 NAME    = roll-$(ROLL)-usersguide
 RELEASE = 0
 
@@ -6,5 +5,5 @@ SUMMARY_COMPATIBLE   = $(VERSION)
 SUMMARY_MAINTAINER   = Rocks Group
 SUMMARY_ARCHITECTURE = i386, x86_64
 
-ROLL_REQUIRES  = base kernel os1 os2 web-server
+ROLL_REQUIRES  = base kernel os web-server
 ROLL_CONFLICTS = 
