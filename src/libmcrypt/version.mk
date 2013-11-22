84de2863959810c4e1ad8fb869add21aa3a496bf
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 15:08:17 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/libmcrypt/version.mk
+++ src/libmcrypt/version.mk
@@ -1,3 +1,15 @@
-NAME    = libmcrypt
-VERSION = 2.5.8
-RELEASE = 1
+NAME               = libmcrypt
+VERSION            = 2.5.8
+RELEASE            = 1
+PKGROOT            = /opt/rocks
+
+SRC_SUBDIR         = libmcrypt
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS           = $(SOURCE_PKG)
+
