4f8aae1f2097cbac011b309cc9c0a142735fa65d
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 15:15:02 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/nagios/version.mk
+++ src/nagios/version.mk
@@ -1,3 +1,15 @@
 NAME    = nagios
 VERSION = 3.4.3
 RELEASE = 0
+PKGROOT = /opt/nagios
+
+SRC_SUBDIR         = nagios
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS           = $(SOURCE_PKG)
+
