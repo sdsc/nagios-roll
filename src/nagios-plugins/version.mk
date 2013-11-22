6f09e4fab0ee19e858674c30ad474f365ca16688
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 15:18:05 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/nagios-plugins/version.mk
+++ src/nagios-plugins/version.mk
@@ -1,3 +1,15 @@
-NAME    = nagios-plugins
-VERSION = 1.4.16
-RELEASE = 1
+NAME               = nagios-plugins
+VERSION            = 1.4.16
+RELEASE            = 1
+PKGROOT            = /opt/nagios
+
+SRC_SUBDIR         = nagios-plugins
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
