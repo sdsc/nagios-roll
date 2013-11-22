29ba6745de7836d0549be21898162a317b46bda3
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 15:21:56 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/nsca/version.mk
+++ src/nsca/version.mk
@@ -1,3 +1,14 @@
-NAME    = nsca
-VERSION = 2.9.1
-RELEASE = 1
+NAME               = nsca
+VERSION            = 2.9.1
+RELEASE            = 1
+PKGROOT            = /opt/nagios
+
+SRC_SUBDIR         = nsca
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
