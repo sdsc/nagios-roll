40e355f95787b234f94d360aca722e338d2d5175
Jim Hayes <jhayes@sdsc.edu>
2018-02-28 08:34:22 -0800

    Change rpm file list values to avoid collisions with nagios package.

--- src/nagios-plugins/version.mk
+++ src/nagios-plugins/version.mk
@@ -14,4 +14,4 @@ SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 TAR_GZ_PKGS    = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No\nAutoProv:No
-RPM.PREFIX     = $(PKGROOT)
+RPM.FILES      = $(PKGROOT)/*/*
